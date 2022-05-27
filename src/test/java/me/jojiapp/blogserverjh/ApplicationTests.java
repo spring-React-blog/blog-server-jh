e01c905d1de879564a9d6dd19f8d9eda761ef070
joji <jojiapp@gmail.com>
2022-05-27 22:27:32 +0900

    init

---
+++ src/test/java/me/jojiapp/blogserverjh/ApplicationTests.java
@@ -0,0 +1,14 @@
+package me.jojiapp.blogserverjh;
+
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.*;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
