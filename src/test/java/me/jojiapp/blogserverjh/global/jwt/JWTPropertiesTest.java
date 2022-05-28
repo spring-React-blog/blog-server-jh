06b3411a89a571fd1c94977eb2553ae7a16a53a6
joji <jojiapp@gmail.com>
2022-05-28 18:03:37 +0900

    feat JWTProperties

---
+++ src/test/java/me/jojiapp/blogserverjh/global/jwt/JWTPropertiesTest.java
@@ -0,0 +1,53 @@
+package me.jojiapp.blogserverjh.global.jwt;
+
+
+import lombok.*;
+import org.junit.jupiter.api.*;
+
+import java.time.*;
+import java.util.*;
+
+import static java.nio.charset.StandardCharsets.*;
+import static org.assertj.core.api.Assertions.*;
+
+public class JWTPropertiesTest {
+
+	private final JWTProperties jwtProperties = new JWTProperties("secret", 1L, 2L);
+
+	@Test
+	@DisplayName("Secret Key 값을 Bytes로 변환하여 반환한다")
+	void toBytesSecretKey() throws Exception {
+		assertThat(jwtProperties.toBytesSecretKey()).isEqualTo("secret".getBytes(UTF_8));
+	}
+
+	@Test
+	@DisplayName("Access Token 만료 시간 조회")
+	void getAccessTokenExpiredDate() throws Exception {
+		// Given
+		val now = new Date();
+
+		// When
+		Date actual = jwtProperties.getAccessTokenExpiredDate(now);
+
+		// Then
+		assertThat(actual).isEqualTo(getActualExpiredDate(now, 1L));
+	}
+
+	@Test
+	@DisplayName("Refresh Token 만료 시간 조회")
+	void getRefreshTokenExpiredDate() throws Exception {
+		// Given
+		val now = new Date();
+
+		// When
+		Date actual = jwtProperties.getRefreshTokenExpiredDate(now);
+
+		// Then
+		assertThat(actual).isEqualTo(getActualExpiredDate(now, 2L));
+	}
+
+	private Date getActualExpiredDate(Date now, Long expiredMinutes) {
+		return new Date(now.getTime() + Duration.ofMinutes(expiredMinutes).toMillis());
+	}
+
+}
