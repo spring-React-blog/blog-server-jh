24a774eea5ec25328b9ded2a96f0064ba7a70a3e
joji <jojiapp@gmail.com>
2022-05-30 01:26:19 +0900

    docs javadoc GlobalException

--- src/main/java/me/jojiapp/blogserverjh/global/exception/GlobalExceptionHandler.java
+++ src/main/java/me/jojiapp/blogserverjh/global/exception/GlobalExceptionHandler.java
@@ -11,11 +11,23 @@ import javax.persistence.*;
 
 import static me.jojiapp.blogserverjh.global.exception.GlobalError.*;
 
+/**
+ * Global Exception Handler
+ */
 @Slf4j
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
+	/**
+	 * 지원하지 않는 메소드를 호출했을 경우 발생하는 예외 처리
+	 *
+	 * @param e 예외
+	 *
+	 * @return 예외 메세지
+	 *
+	 * @see APIResponse
+	 */
 	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
 	@ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
 	private APIResponse<Void> httpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
@@ -23,6 +35,15 @@ public class GlobalExceptionHandler {
 		return APIResponse.error(METHOD_NOT_ALLOWED.getMessage());
 	}
 
+	/**
+	 * 잘못된 요청으로 예외가 발생하는 경우 예외 처리
+	 *
+	 * @param e 예외
+	 *
+	 * @return 예외 메세지
+	 *
+	 * @see APIResponse
+	 */
 	@ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class})
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	private APIResponse<Void> badRequestException(RuntimeException e) {
@@ -30,6 +51,15 @@ public class GlobalExceptionHandler {
 		return APIResponse.error(e.getMessage());
 	}
 
+	/**
+	 * 엔티티가 존재하지 않는 경우 발생하는 예외 처리
+	 *
+	 * @param e 예외
+	 *
+	 * @return 예외 메세지
+	 *
+	 * @see APIResponse
+	 */
 	@ExceptionHandler(EntityNotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	private APIResponse<Void> entityNotFoundException(EntityNotFoundException e) {
@@ -37,6 +67,15 @@ public class GlobalExceptionHandler {
 		return APIResponse.error(ENTITY_NOT_FOUND.getMessage());
 	}
 
+	/**
+	 * 서버에서 처리하지 못한 예외가 발생하는 경우 예외 처리
+	 *
+	 * @param e 예외
+	 *
+	 * @return 예외 메세지
+	 *
+	 * @see APIResponse
+	 */
 	@ExceptionHandler(Exception.class)
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	private APIResponse<Void> exception(Exception e) {
