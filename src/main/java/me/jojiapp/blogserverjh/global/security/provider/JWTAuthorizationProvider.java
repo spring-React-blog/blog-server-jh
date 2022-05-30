5296276d5fd9004f1f2dcd43bcb6270a88b623f4
joji <jojiapp@gmail.com>
2022-05-31 02:06:30 +0900

    docs javadoc JWTAuthorizationProvider

--- src/main/java/me/jojiapp/blogserverjh/global/security/provider/JWTAuthorizationProvider.java
+++ src/main/java/me/jojiapp/blogserverjh/global/security/provider/JWTAuthorizationProvider.java
@@ -1,5 +1,6 @@
 package me.jojiapp.blogserverjh.global.security.provider;
 
+import io.jsonwebtoken.*;
 import lombok.*;
 import me.jojiapp.blogserverjh.global.jwt.*;
 import me.jojiapp.blogserverjh.global.jwt.error.*;
@@ -17,14 +18,46 @@ import org.springframework.security.core.*;
 @RequiredArgsConstructor
 public class JWTAuthorizationProvider implements AuthenticationProvider {
 
+	/**
+	 * JWT Provider
+	 */
 	private final JWTProvider jwtProvider;
 
+	/**
+	 * JWTAccessTokenAuthentication 객체를 전달받아
+	 * principal에 담겨있는 Access Token 기반 정보로 UsernamePasswordAuthenticationToken 객체를 생성하여 반환
+	 *
+	 * @param authentication JWTAccessTokenAuthentication
+	 *
+	 * @return UsernamePasswordAuthenticationToken
+	 *
+	 * @throws AuthenticationException
+	 * @see Authentication
+	 * @see JWTAccessTokenAuthentication
+	 */
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 		val accessToken = (String) authentication.getPrincipal();
 		if (!jwtProvider.isAccessToken(accessToken))
 			throw new JWTAuthenticationException(JWTError.NOT_ACCESS_TOKEN);
+		try {
+			return createAuthenticationByAccessToken(accessToken);
+		} catch (JwtException e) {
+			throw new JWTAuthenticationException(e.getMessage(), e);
+		}
 
+	}
+
+	/**
+	 * Access Token으로 Authentication 객체를 생성
+	 *
+	 * @param accessToken 인증에 필요한 Access Token
+	 *
+	 * @return Authentication
+	 *
+	 * @see Authentication
+	 */
+	private Authentication createAuthenticationByAccessToken(String accessToken) {
 		return new UsernamePasswordAuthenticationToken(
 				jwtProvider.getIssuer(accessToken),
 				null,
@@ -32,6 +65,11 @@ public class JWTAuthorizationProvider implements AuthenticationProvider {
 		);
 	}
 
+	/**
+	 * @param authentication 인증 객체
+	 *
+	 * @return JWTAccessTokenAuthentication 클래스를 지원하는지 판단 여부
+	 */
 	@Override
 	public boolean supports(Class<?> authentication) {
 		return authentication.isAssignableFrom(JWTAccessTokenAuthentication.class);
