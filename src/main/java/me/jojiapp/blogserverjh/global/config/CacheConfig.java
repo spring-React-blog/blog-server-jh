f7438e8a0ee0add58ee093ab52de39f229f22ca0
joji <jojiapp@gmail.com>
2022-05-31 01:55:25 +0900

    feat redis config

---
+++ src/main/java/me/jojiapp/blogserverjh/global/config/CacheConfig.java
@@ -0,0 +1,41 @@
+package me.jojiapp.blogserverjh.global.config;
+
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.datatype.jsr310.*;
+import lombok.*;
+import org.springframework.cache.*;
+import org.springframework.context.annotation.*;
+import org.springframework.data.redis.cache.*;
+import org.springframework.data.redis.connection.*;
+import org.springframework.data.redis.serializer.*;
+
+import java.time.*;
+
+@Configuration
+@RequiredArgsConstructor
+public class CacheConfig {
+
+	private final RedisConnectionFactory redisConnectionFactory;
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper()
+				.findAndRegisterModules()
+				.enable(SerializationFeature.INDENT_OUTPUT)
+				.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+				.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+				.registerModule(new JavaTimeModule());
+	}
+
+	@Bean
+	public CacheManager cacheManager() {
+		RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+				.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+				.serializeValuesWith(RedisSerializationContext.
+						SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper())))
+				.entryTtl(Duration.ofSeconds(30));
+
+		return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory)
+				.cacheDefaults(redisCacheConfiguration).build();
+	}
+}
---
+++ src/main/java/me/jojiapp/blogserverjh/global/config/RedisConfig.java
@@ -0,0 +1,35 @@
+package me.jojiapp.blogserverjh.global.config;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.context.annotation.*;
+import org.springframework.data.redis.connection.*;
+import org.springframework.data.redis.connection.lettuce.*;
+import org.springframework.data.redis.core.*;
+import org.springframework.data.redis.serializer.*;
+
+@Configuration
+public class RedisConfig {
+	@Value("${spring.redis.host}")
+	private String host;
+
+	@Value("${spring.redis.port}")
+	private int port;
+
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+		redisStandaloneConfiguration.setHostName(host);
+		redisStandaloneConfiguration.setPort(port);
+		return new LettuceConnectionFactory(redisStandaloneConfiguration);
+	}
+
+	@Bean
+	public RedisTemplate<?, ?> redisTemplate() {
+		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		return redisTemplate;
+	}
+}
